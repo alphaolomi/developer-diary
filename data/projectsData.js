d36a69ac1e7494ab1fdf2931c2f11ef4a4da9756
Alpha Olomi <alphaolomi@gmail.com>
2022-05-19 19:28:34 +0300

    wip

---
+++ data/projectsData.js
@@ -0,0 +1,11 @@
+const projectsData = [
+  {
+    title: 'Openpesa',
+    description: `for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+ 
+]
+
+export default projectsData
